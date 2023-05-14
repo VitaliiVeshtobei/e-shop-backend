1e19b86d8edf5680e0091d623833bc6359614ff1
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-05-14 20:18:11 +0300

    add in scheme date create

--- src/scheme/category.ts
+++ src/scheme/category.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const categoryScheme = new Schema({
-  image: { type: String, required: true },
-  name: { type: String, required: false },
-});
+const categoryScheme = new Schema(
+  {
+    image: { type: String, required: true },
+    name: { type: String, required: false },
+  },
+  { versionKey: false, timestamps: true },
+);
 
 export const Category = mongoose.model('Category', categoryScheme);
